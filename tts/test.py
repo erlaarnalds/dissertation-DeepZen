5aa83cd81e5a725d8a8e2d63e3a7c1017356df1f
Erla <arnalds3@gmail.com>
2024-09-02 15:18:11 +0000

    adding comparison for dilja and mms_finetuned

--- tts/test.py
+++ tts/test.py
@@ -11,13 +11,20 @@ def convert_wav_to_mp3(wav_file_path, mp3_file_path):
     print(f"Converted {wav_file_path} to {mp3_file_path}")
 
 def convert_all_wav_to_mp3(directory):
-    for filename in os.listdir(directory):
-        if filename.endswith(".wav"):
-            wav_file_path = os.path.join(directory, filename)
-            mp3_dir = os.path.join(f"{directory}/mp3/", filename)
-            mp3_file_path = os.path.splitext(mp3_dir)[0] + ".mp3"
-            convert_wav_to_mp3(wav_file_path, mp3_file_path)
+    i = 0
+    for base_filename in os.listdir(directory):
+        if base_filename.startswith("s"):
+            if i == 10:
+                break
+            i += 1
+            synth_file_name = f"mms_finetuned_{base_filename}"
 
-# Example usage
-directory_path = "voice_samples/audio"  # Replace with the directory containing your WAV files
+            for filename in [base_filename, synth_file_name]:
+                wav_file_path = os.path.join(directory, filename)
+                mp3_dir = os.path.join(f"voice_samples/audio/dilja/", filename)
+                mp3_file_path = os.path.splitext(mp3_dir)[0] + ".mp3"
+                convert_wav_to_mp3(wav_file_path, mp3_file_path)
+
+# Example
+directory_path = "secs_evaluation_samples/dilja"  # Replace with the directory containing your WAV files
 convert_all_wav_to_mp3(directory_path)
